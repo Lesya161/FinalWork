ecf8c490801496e3bcd29666f0697fea15990ec4
Lesya <neptun96@mail.ru>
2022-11-26 17:44:23 +0300

    added metod for create array

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string [] CreateStringArray(int size)
+{
+    string [] array = new string [size];
+    for(int i = 0; i < size; i++)
+    {
+        Console.WriteLine("Write a {i + 1} element of your array: ");
+        array[i] = Convert.ToString(Console.ReadLine());
+    }
+    return array;
+}
