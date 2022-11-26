f77d8d42353b2b66599f109e61ad85f884160497
Lesya <neptun96@mail.ru>
2022-11-26 17:55:51 +0300

    correction of the method

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@
     string [] array = new string [size];
     for(int i = 0; i < size; i++)
     {
-        Console.WriteLine("Write a {i + 1} element of your array: ");
+        Console.WriteLine($"Write a {i + 1} element of your array: ");
         array[i] = Convert.ToString(Console.ReadLine());
     }
     return array;
